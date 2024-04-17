b91d8f3a062265627e93fa63d431866c20ee6075
NohSiHeon <shtlgjs016@naver.com>
2024-04-17 19:00:59 +0900

    메뉴바 스크롤 구현

--- scrollBtn.js
+++ scrollBtn.js
@@ -1,7 +1,72 @@
 export function scrollTop() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 }
 
-const scrollBtn = document.getElementById("scrollBtn");
-
+let scrollBtn = document.getElementById("scrollBtn");
 scrollBtn.addEventListener("click", scrollTop);
+
+// 기본 정보
+function scrollbasicInfo() {
+
+    window.scrollTo({
+        top: basicInfo.offsetTop,
+        behavior: "smooth"
+    });
+}
+let basicInfo = document.getElementById('basicInfo');
+item_basicInfo.addEventListener('click', scrollbasicInfo);
+
+// MBTI
+function scrollmbti() {
+
+
+    window.scrollTo({
+        top: mbti.offsetTop,
+        behavior: "smooth"
+    });
+}
+
+let mbti = document.getElementById('mbti');
+item_mbti.addEventListener('click', scrollmbti);
+
+// 나의 장점
+function scrollStrength() {
+
+
+    window.scrollTo({
+        top: strength.offsetTop,
+        behavior: "smooth"
+    });
+}
+
+let strength = document.getElementById('strength');
+item_strength.addEventListener('click', scrollStrength);
+
+// 협업 스타일
+function scrollWorkingStyle() {
+
+
+    window.scrollTo({
+        top: workingStyle.offsetTop,
+        behavior: "smooth"
+    });
+}
+
+let workingStyle = document.getElementById('workingStyle');
+item_workingStyle.addEventListener('click', scrollWorkingStyle);
+
+// 취미
+function scrollHobby() {
+
+    window.scrollTo({
+        top: hobby.offsetTop,
+        behavior: "smooth"
+    });
+
+    
+}
+let hobby = document.getElementById('hobby');
+    item_hobby.addEventListener('click', scrollHobby);
